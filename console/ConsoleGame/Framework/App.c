0bd1118ecb1ec956ba48da2c30ef119c16c0c70e
KimDongHyun <qwas4689@gmail.com>
2022-05-11 09:21:53 +0900

    타이머 구현

--- console/ConsoleGame/Framework/App.c
+++ console/ConsoleGame/Framework/App.c
@@ -1,6 +1,7 @@
 #include "App.h"
 #include "Common.h"
 #include "Renderer.h"
+#include "Timer.h"
 
 bool App_Init()
 {
@@ -17,9 +18,36 @@ void processInput()
 
 }
 
+float elapsedTime;
+bool canShow = false;
 void update()
 {
-    Renderer_DrawText("Hello Game", sizeof("Hello Game"));
+    /*
+    // 비례식
+    // 프레임 : 시간
+    // 1 : deltaTime = fps : 1
+    // 1 = deltaTime * fps
+    // fps = 1 / deltaTime
+    char str[128] = "";
+    sprintf_s(str, sizeof(str), "현재 FPS : %d", (int32)(1 / Timer_GetDeltaTime()));
+    Renderer_DrawText(str, strlen(str));
+    */
+
+    // 0.5초 마다 깜빡이는 텍스트를 만드시오.
+    elapsedTime += DELTA_TIME;
+
+    if (elapsedTime >= 0.5f)
+    {
+        elapsedTime = 0.0f;
+        canShow = !canShow;
+    }
+
+    if (canShow)
+    {
+        Renderer_DrawText("이 텍스트는 깜빡입니다.", sizeof("이 텍스트는 깜빡입니다."));
+    }
+    
+
 }
 
 void render()
@@ -36,12 +64,19 @@ int32 App_Run()
 {
     atexit(cleanup);
 
+    Timer_Init(60);
+
+
     // Game Loop : 게임을 동작시키는 루프. 프레임(Frame)이라고 한다.
     while (true)
     {
-        processInput();      // 입력 처리
-        update();         // 게임 업데이트
-        render();         // 게임 출력
+        // 시간이 업데이트 됐을 때에만 게임 루프 실행
+        if (Timer_Update()) 
+        {
+            processInput();      // 입력 처리
+            update();         // 게임 업데이트
+            render();         // 게임 출력
+        }
     }
 
     return 0;
